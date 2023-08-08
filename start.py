fc5d0d6a1b0000b6b7bafb2332b9ecf20fd72417
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-08-08 15:35:15 -0500

    installed pandas

---
+++ start.py
@@ -0,0 +1,12 @@
+import pandas as pd # this imports the pandas library and renames it pd fr short
+
+#Define a dictionary 'x'
+
+x = {'Name': ['Rose','John', 'Jane', 'Mary'], 'ID': [1, 2, 3, 4], 'Department': ['Architect Group', 'Software Group', 'Design Team', 'Infrastructure'], 
+      'Salary':[100000, 80000, 50000, 60000]}
+
+#casting the dictionary to a DataFrame
+df = pd.DataFrame(x)
+
+#display the result df
+print(df)
